d9144892fb2134959413c4784a3addfdb26161df
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-10-01 15:55:32 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,15 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista: list[str]):
+    resultado = []
+    for i, elemento in enumerate(lista):
+        if not elemento:
+            continue
+        resultado.append(f"{i}. {elemento}")
+    return resultado
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista: list[str]):
+    resultado = []
+    for i, elemento in enumerate(lista):
+        if not elemento:
+            continue
+        resultado.append(f"{i}. {elemento[::-1]}")
+    return resultado
