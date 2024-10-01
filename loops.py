59fe40eace2987c9393cdb42d3112f5ada53aecc
JoacoDerito <162723144+JoacoDerito@users.noreply.github.com>
2024-10-01 15:56:03 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,32 @@
-def index_of_by_index(word, list, index):
+def find_from_index(word: str, lista: list[str], index: int):
+    for idx, el in enumerate(lista):
+        if idx >= index and el == word:
+            return idx
     return -1
 
-
-def index_of_empty(list):
+def index_of_empty(lista: list[str]):
+    for idx, el in enumerate(lista):
+        if el == "":
+            return idx
     return -1
 
-
-def index_of(word, list):
+def index_of(word: str, lista: list[str]):
+    for idx, el in enumerate(lista):
+        if el == word:
+            return idx
     return -1
 
-
-def put(word, list):
+def poner(palabra: str, lista: list[str]):
+    for idx, el in enumerate(lista):
+        if el == "":
+            lista[idx] = palabra
+            return idx
     return -1
 
-
-def remove(word, list):
-    return -1
+def eliminar(palabra: str, lista: list[str]):
+    contador = 0
+    for idx, el in enumerate(lista):
+        if el == palabra:
+            lista[idx] = ""
+            contador += 1
+    return contador
